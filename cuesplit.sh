351ba0016d959d3716061d4cfb50d8f4f269f058
Paul <paul70079@gmail.com>
2020-02-22 23:08:42 +0100

    cuetag.sh is cuetag with ubuntu

--- cuesplit.sh
+++ cuesplit.sh
@@ -28,45 +28,45 @@ function _cuesplit {
         mkdir -p "$2"
         shnsplit -d "$2" -f "$1.cue" -o "flac flac -V --best -o %f -" "$1.ape" -t "%n %p - %t"
         rm -f "$2/00*pregap*"
-        cuetag.sh "$1.cue" "$2/*.flac"
+        cuetag "$1.cue" "$2/*.flac"
 
     elif [ -f "$1.flac" ]
     then
         mkdir -p "$2"
         shnsplit -d "$2" -f "$1.cue" -o "flac flac -V --best -o %f -" "$1.flac" -t "%n %p - %t"
         rm -f "$2/00*pregap*"
-        cuetag.sh "$1.cue" "$2/*.flac"
+        cuetag "$1.cue" "$2/*.flac"
 
     elif [ -f "$1.mp3" ]
     then
         mp3splt -no "@n @p - @t" -c "$1.cue" -d "$2" "$1.mp3"
-        cuetag.sh "$1.cue" "$2/*.mp3"
+        cuetag "$1.cue" "$2/*.mp3"
 
     elif [ -f "$1.ogg" ]
     then
         mp3splt -no "@n @p - @t" -c "$1.cue" -d "$2" "$1.ogg"
-        cuetag.sh "$1.cue" "$2/*.ogg"
+        cuetag "$1.cue" "$2/*.ogg"
 
     elif [ -f "$1.tta" ]
     then
         mkdir -p "$2"
         shnsplit -d "$2" -f "$1.cue" -o "flac flac -V --best -o %f -" "$1.tta" -t "%n %p - %t"
         rm -f "$2/00*pregap*"
-        cuetag.sh "$1.cue" "$2/*.flac"
+        cuetag "$1.cue" "$2/*.flac"
 
     elif [ -f "$1.wv" ]
     then
         mkdir -p "$2"
         shnsplit -d "$2" -f "$1.cue" -o "flac flac -V --best -o %f -" "$1.wv" -t "%n %p - %t"
         rm -f "$2/00*pregap*"
-        cuetag.sh "$1.cue" "$2/*.flac"
+        cuetag "$1.cue" "$2/*.flac"
 
     elif [ -f "$1.wav" ]
     then
         mkdir -p "$2"
         shnsplit -d "$2" -f "$1.cue" -o "flac flac -V --best -o %f -" "$1.wav" -t "%n %p - %t"
         rm -f "$2/00*pregap*"
-        cuetag.sh "$1.cue" "$2/*.flac"
+        cuetag "$1.cue" "$2/*.flac"
 
     else
         echo "Error: Found no files to split!"
